9bff435ea9c42ceaf631b594cd8272b5580a5db2
natalia <natalia1528alonso@gmail.com>
2020-03-31 17:31:25 +0200

    actualizado crear nodos y edges

--- 1.crear_edges.py
+++ 1.crear_edges.py
@@ -3,29 +3,28 @@ import csv
 
 # Crea las aristas/estaciones contiguas: estacion_a-estacion_b
 
-with open('grafo_edges.csv', 'w', newline='', encoding="utf-8-sig") as edges:
-    write_csv = csv.writer(edges, delimiter=",")
-    csvFile = open("resultado_limpio_metro.csv", "r", encoding= "utf-8")
-    csvReader = csv.reader(csvFile, delimiter=',', lineterminator='\n')
+edges = open('grafo_edges.csv', 'w', newline='', encoding="utf-8-sig")
+write_csv = csv.writer(edges, delimiter=",")
+csvFile = open("resultado_limpio_metro.csv", "r", encoding= "utf-8")
+csvReader = csv.reader(csvFile, delimiter=',', lineterminator='\n')
 
-    line_number = []
-    stop_id = []
-    stop_name = []
+line_number = []
+stop_id = []
+stop_name = []
 
-    for row in csvReader:
-        line_number.append(row[1])
-        stop_id.append(row[3])
-        stop_name.append(row[5])
+for row in csvReader:
+    line_number.append(row[1])
+    stop_id.append(row[3])
+    stop_name.append(row[5])
 
-    line = 0
-    for line in range(len(line_number)):
-
-        if line_number[line] != 'line_number' and line < 290 and str(line_number[line]) == str(line_number[line+1]):
-            write_csv.writerow([stop_id[line], stop_id[line + 1]])
-            if stop_id[line + 1] == 'par_4_130':
-                write_csv.writerow([stop_id[line + 1], 'par_4_104'])
-            if stop_id[line + 1] == 'par_4_236':
-                write_csv.writerow([stop_id[line + 1], 'par_4_205'])
+line = 0
+for line in range(len(line_number)):
+    if line_number[line] != 'line_number' and line < (len(line_number)-1) and str(line_number[line]) == str(line_number[line+1]):
+        write_csv.writerow([stop_id[line], stop_id[line + 1]])
+        if stop_id[line + 1] == 'par_4_130':
+            write_csv.writerow([stop_id[line + 1], 'par_4_104'])
+        if stop_id[line + 1] == 'par_4_236':
+            write_csv.writerow([stop_id[line + 1], 'par_4_205'])
 
 print("terminado TODO")
 
--- 2.crear_edges_nombres.py
+++ 2.crear_edges_nombres.py
@@ -1,27 +1,27 @@
 
 import csv
 
-with open('grafo_edges_names.csv', 'w', newline='', encoding="utf-8-sig") as edges_names:
-    write_csv = csv.writer(edges_names, delimiter=",")
-    csvFile = open("resultado_limpio_metro.csv", "r", encoding= "utf-8")
-    csvReader = csv.reader(csvFile, delimiter=',', lineterminator='\n')
+edges_names = open('grafo_edges_names.csv', 'w', newline='', encoding="utf-8-sig")
+write_csv = csv.writer(edges_names, delimiter=",")
+csvFile = open("resultado_limpio_metro.csv", "r", encoding= "utf-8")
+csvReader = csv.reader(csvFile, delimiter=',', lineterminator='\n')
 
-    line_number = []
-    stop_id = []
-    stop_name = []
+line_number = []
+stop_id = []
+stop_name = []
 
-    for row in csvReader:
-        line_number.append(row[1])
-        stop_id.append(row[3])
-        stop_name.append(row[5])
+for row in csvReader:
+    line_number.append(row[1])
+    stop_id.append(row[3])
+    stop_name.append(row[5])
 
-    line = 0
-    for line in range(len(line_number)):
-        if line_number[line] != 'line_number' and line < 290 and str(line_number[line]) == str(line_number[line+1]):
-            write_csv.writerow([stop_name[line], stop_name[line + 1]])
-            if stop_name[line + 1] == 'LUCERO':
-                write_csv.writerow([stop_name[line + 1], 'LAGUNA'])
-            if stop_name[line + 1] == 'SAN NICASIO':
-                write_csv.writerow([stop_name[line + 1], 'PUERTA DEL SUR'])
+line = 0
+for line in range(len(line_number)):
+    if line_number[line] != 'line_number' and line < (len(line_number)-1) and str(line_number[line]) == str(line_number[line+1]):
+        write_csv.writerow([stop_name[line], stop_name[line + 1]])
+        if stop_name[line + 1] == 'LUCERO':
+            write_csv.writerow([stop_name[line + 1], 'LAGUNA'])
+        if stop_name[line + 1] == 'SAN NICASIO':
+            write_csv.writerow([stop_name[line + 1], 'PUERTA DEL SUR'])
 
 print("terminado TODO")
