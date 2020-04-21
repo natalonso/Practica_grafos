a62a2647ef3a8d5701577381c1d318297dfe793e
natalia <natalia1528alonso@gmail.com>
2020-04-21 13:57:11 +0200

    añadido pruebas d3js

--- 4.crear_nodes.py
+++ 4.crear_nodes.py
@@ -9,16 +9,22 @@ csvReader = csv.reader(csvFile, delimiter=',', lineterminator='\n')
 line_number = []
 stop_id = []
 stop_name = []
+stop_lat = []
+stop_lon = []
+
 
 for row in csvReader:
     line_number.append(row[1])
     stop_id.append(row[3])
     stop_name.append(row[5])
+    stop_lat.append(row[7])
+    stop_lon.append(row[8])
+
 
 line = 0
 for line in range(len(line_number)):
     if line_number[line] != 'line_number' and line < (len(line_number)-1) and str(line_number[line]) == str(line_number[line+1]):
-        write_csv.writerow([stop_id[line], stop_name[line]])
-write_csv.writerow([stop_id[line], stop_name[line]])
+        write_csv.writerow([stop_id[line], stop_name[line], stop_lat[line], stop_lon[line], line_number[line]])
+write_csv.writerow([stop_id[line], stop_name[line], stop_lat[line], stop_lon[line], line_number[line]])
 
 print("terminado TODO")
--- 5.main.py
+++ 5.main.py
@@ -1,18 +1,45 @@
 
 import networkx as nx
+import networkx.algorithms.community as nxComm
+import json
+from networkx.readwrite import json_graph
+
 import matplotlib.pyplot as plt
 import csv
 
 
 g = nx.read_edgelist('grafo_edges_names.csv', delimiter=',', encoding="utf-8")
 
-# print(nx.info(g))
-# print(g.edges(data = True))
-# print(g.nodes(data=True))
+print(nx.info(g))
+print(g.edges(data = True))
+print(g.nodes(data=True))
 
-# plt
+plt
 nx.draw(g, with_labels=True)
 plt.show()
+
+
+################################################
+# ASIGNAR ATRIBUTOS A LOS NODOS:
+################################################
+
+atributos = open("grafo_nodes.csv", "r", encoding= "utf-8")
+csvReader = csv.reader(atributos, delimiter=',', lineterminator='\n')
+
+
+atributos=[]
+for line in csvReader:
+    atributos.append(line)
+
+nodes = g.nodes(data = True)
+
+for node in nodes:
+    for line in atributos:
+        if line[1] == node[0]:
+            g.nodes[node[0]]["lat"] = line[2]
+            g.nodes[node[0]]["lon"] = line[3]
+            g.nodes[node[0]]["line"] = line[4]
+
 
 ########################
 # ADYACENCIA:
@@ -168,11 +195,6 @@ plt.show()
 # print("PageRank: " + bestPR + " -> {0:.2f}".format(pagerank[bestPR]))
 
 
-################################################
-# Line graph:
-################################################
-
-# Transformar nodos en aristas y al reves ----> no nos aporta nada
 
 ################################################
 # Edges Betweeness:
@@ -181,6 +203,13 @@ plt.show()
 # centrality = nx.edge_betweenness_centrality(g)
 # for node in centrality:
 #     print("{0:s}-{1:s}({2:.2f})".format(node[0], node[1], centrality[node]), end='\n')
+
+################################################
+# Line graph:
+################################################
+
+# Transformar nodos en aristas y al reves ----> no nos aporta nada
+
 
 ################################################
 # Cliques:
@@ -192,6 +221,12 @@ plt.show()
 # print("Todos los cliques maximos:")
 # for max_clique in nx.find_cliques(g):
 #     print(max_clique)
+
+# lista_cliques = list(nx.find_cliques(g))
+# print('Cliques mayores que dos: ')
+# for i in range(len(lista_cliques)):
+#     if len(lista_cliques[i]) > 2:
+#         print(lista_cliques[i])
 
 ################################################
 # k-core:
@@ -206,3 +241,173 @@ plt.show()
 # print(len(kg))
 # nx.draw(kg, with_labels=True)
 # plt.show()
+
+################################################
+# coeficiente de clustering:
+################################################
+
+# Número de "triangulos que hay" entre los que podrían crearse si unimos algunos nodos
+# Nos de como resultado 0.0333: podría concluirse que tenemos 5 cliques (3 nodos)  / 150 que se podrían conseguirse
+
+# print("Coeficiente clustering: " + str(nx.transitivity(g)))
+
+################################################
+# reciprocidad:
+################################################
+
+# para grafos dirigidos: no tiene sentido en nuestro caso
+
+################################################
+# Conectividad o componentes conexas:
+################################################
+
+# Se considerará una conectividad óptima cuando la comunicación entre dos vértices o nodos se realiza a través de un sólo salto
+# En el caso del metro lo normal es que no todas las estaciones estén conectadas con un solo salto al resto de estaciones.
+
+# El grado de conectividad se utiliza como indicador de desarrollo de los países al existir una
+# relación directa entre el grado de conectividad de la red de transporte de un Estado y su renta per cápita.
+
+# Media de caminos más cortos, de cualquier vértice a cualquier vértice
+# print("AVG LENGTH: " + str(nx.average_shortest_path_length(g)))
+
+# Camino más largo en el grafo (entre las dos estaciones más alejadas)
+# print("DIAMETER: " + str(nx.diameter(g)))
+
+################################################
+# Deteccion de comunidades:
+################################################
+
+# La modularidad es una medida de la estructura de las redes o grafos.
+# Fue diseñado para medir la fuerza de la división de una red en módulos (también llamados grupos, agrupamientos o comunidades).
+
+pos=nx.spring_layout(g) # positions for all nodes
+
+commGreedy = list(nxComm.greedy_modularity_communities(g)) #----> este podría mostrarnos el "centro" como una "comunidad"
+# con el greedy vemos los extremos de las líneas (poca densidad) y "el centro de la ciudad" con más densidad
+
+# commLabel = list(nxComm.label_propagation_communities(g)) ---> este en nuestro caso no tendría sentido
+
+# print("GREEDY")
+# for c in commGreedy:
+#     print(sorted(c))
+
+# print("LABEL")
+# for c in commLabel:
+#     print(sorted(c))
+
+# print("COVERAGE:")
+# print("\tGREEDY: "+str(nxComm.coverage(g, commGreedy)))
+# print("\tLABEL: "+str(nxComm.coverage(g, commLabel)))
+# print("PERFORMANCE:")
+# print("\tGREEDY: "+str(nxComm.performance(g, commGreedy)))
+# print("\tLABEL: "+str(nxComm.performance(g, commLabel)))
+# print("MODULARITY")
+# print("\tGREEDY: "+str(nxComm.modularity(g, commGreedy)))
+# print("\tLABEL: "+str(nxComm.modularity(g, commLabel)))
+
+
+# nodes
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALCORCON CENTRAL', 'ALONSO DE MENDOZA', 'ARROYO CULEBRO', 'AVIACION ESPAÑOLA', 'CASA DEL RELOJ', 'COLONIA JARDIN', 'CONSERVATORIO', 'CUATRO VIENTOS', 'EL BERCIAL', 'EL CARRASCAL', 'EL CASAR', 'FUENLABRADA CENTRAL', 'GETAFE CENTRAL', 'HOSPITAL DE FUENLABRADA', 'HOSPITAL DE MOSTOLES', 'HOSPITAL SEVERO OCHOA', 'JOAQUIN VILUMBRALES', 'JUAN DE LA CIERVA', 'JULIAN BESTEIRO', 'LEGANES CENTRAL', 'LORANCA', 'LOS ESPARTALES', 'MOSTOLES CENTRAL', 'Manuela Malasaña', 'PARQUE DE LOS ESTADOS', 'PARQUE EUROPA', 'PARQUE LISBOA', 'PARQUE OESTE', 'PRADILLO', 'PUERTA DEL SUR', 'SAN NICASIO', 'UNIVERSIDAD REY JUAN CARLOS'],
+                       node_color='orange',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALSACIA', 'Avenida de Guadalajara', 'BANCO DE ESPAÑA', 'COLON', 'CONDE DE CASAL', 'DIEGO DE LEON', 'EL CARMEN', 'GOYA', 'IBIZA', 'LA ALMUDENA', 'LA ELIPA', 'LAS ROSAS', 'LISTA', 'MANUEL BECERRA', 'NUÑEZ DE BALBOA', 'ODONNELL', 'PRINCIPE DE VERGARA', 'QUINTANA', 'RETIRO', 'RUBEN DARIO', 'SAINZ DE BARANDA', 'SERRANO', 'SEVILLA', 'VELAZQUEZ', 'VENTAS'],
+                       node_color='red',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALAMEDA DE OSUNA', 'ASCAO', 'BARRIO DE LA CONCEPCION', 'BARRIO DEL PUERTO', 'CANILLEJAS', 'CARTAGENA', 'CIUDAD LINEAL', 'COSLADA CENTRAL', 'EL CAPRICHO', 'ESTADIO METROPOLITANO', 'GARCIA NOBLEJAS', 'HENARES', 'HOSPITAL DEL HENARES', 'JARAMA', 'LA RAMBLA', 'LAS MUSAS', 'PARQUE DE LAS AVENIDAS', 'PUEBLO NUEVO', 'SAN BLAS', 'SAN FERNANDO', 'SIMANCAS', 'SUANZES', 'TORRE ARIAS'],
+                       node_color='green',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALONSO MARTINEZ', 'ALTO DE EXTREMADURA', 'ARGÜELLES', 'BILBAO', 'CALLAO', 'CHUECA', 'CIUDAD UNIVERSITARIA', 'GRAN VIA', 'IGLESIA', 'MONCLOA', 'NOVICIADO', 'OPERA', 'PLAZA DE ESPAÑA', 'PRINCIPE PIO', 'PUERTA DEL ANGEL', 'QUEVEDO', 'RIOS ROSAS', 'SAN BERNARDO', 'SANTO DOMINGO', 'SOL', 'TRIBUNAL', 'VENTURA RODRIGUEZ'],
+                       node_color='blue',
+                       node_size=500,
+                   alpha=0.8)
+
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALONSO CANO', 'ALVARADO', 'AVENIDA DE AMERICA', 'CANAL', 'COLOMBIA', 'CONCHA ESPINA', 'CRUZ DEL RAYO', 'CUATRO CAMINOS', 'CUZCO', 'DUQUE DE PASTRANA', 'ESTRECHO', 'GREGORIO MARAÑON', 'Guzmán el Bueno', 'ISLAS FILIPINAS', 'NUEVOS MINISTERIOS', 'PIO XII', 'PLAZA DE CASTILLA', 'REPUBLICA ARGENTINA', 'SANTIAGO BERNABEU', 'TETUAN', 'VALDEACEDERAS', 'VICENTE ALEIXANDRE'],
+                       node_color='sienna',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALTO DEL ARENAL', 'ANTON MARTIN', 'ARGANZUELA-PLANETARIO', 'ATOCHA-RENFE', 'BUENOS AIRES', 'CONGOSTO', 'ESTACION DEL ARTE', 'LA GAVIA', 'LAS SUERTES', 'MENDEZ ALVARO', 'MENENDEZ PELAYO', 'MIGUEL HERNANDEZ', 'NUEVA NUMANCIA', 'PACIFICO', 'PORTAZGO', 'PUENTE DE VALLECAS', 'SIERRA DE GUADALUPE', 'TIRSO DE MOLINA', 'VALDECARROS', 'VILLA DE VALLECAS'],
+                       node_color='gold',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ABRANTES', 'ACACIAS', 'CARABANCHEL ALTO', 'CARPETANA', 'LA FORTUNA', 'LA LATINA', 'LA PESETA', 'LAGUNA', 'LUCERO', 'MARQUES DE VADILLO', 'OPAÑEL', 'OPORTO', 'PAN BENDITO', 'PIRAMIDES', 'PLAZA ELIPTICA', 'PUERTA DE TOLEDO', 'San Francisco', 'URGEL', 'USERA'],
+                       node_color='m',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['AEROPUERTO T1 T2 T3', 'AEROPUERTO T4', 'ALFONSO XIII', 'AVENIDA DE LA PAZ', 'Arturo Soria', 'BARAJAS', 'CANILLAS', 'ESPERANZA', 'FERIA DE MADRID', 'HORTALEZA', 'MANOTERAS', 'MAR DE CRISTAL', 'PARQUE DE SANTA MARIA', 'PINAR DE CHAMARTIN', 'PINAR DEL REY', 'PROSPERIDAD', 'SAN LORENZO'],
+                       node_color='coral',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['BAMBU', 'BAUNATAL', 'BEGOÑA', 'CHAMARTIN', 'Fuencarral', 'HOSPITAL INFANTA SOFÍA', 'LA GRANJA', 'LA MORALEJA', 'LAS TABLAS', 'MANUEL DE FALLA', 'MARQUES DE LA VALDAVIA', 'MONTECARMELO', 'REYES CATOLICOS', 'RONDA DE LA COMUNICACION', 'TRES OLIVOS', '\ufeffPINAR DE CHAMARTIN'],
+                       node_color='khaki',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ARGANDA DEL REY', 'ARTILLEROS', 'ESTRELLA', 'LA POVEDA', 'PAVONES', 'PUERTA DE ARGANDA', 'RIVAS FUTURA', 'RIVAS URBANIZACIONES', 'RIVAS VACIAMADRID', 'SAN CIPRIANO', 'VALDEBERNARDO', 'VICALVARO', 'VINATEROS'],
+                       node_color='cyan',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALMENDRALES', 'CIUDAD DE LOS ANGELES', 'DELICIAS', 'EMBAJADORES', 'HOSPITAL 12 DE OCTUBRE', 'LAVAPIES', 'LEGAZPI', 'PALOS DE LA FRONTERA', 'SAN CRISTOBAL', 'SAN FERMIN-ORCASUR', 'VILLAVERDE ALTO', 'VILLAVERDE BAJO-CRUCE'],
+                       node_color='indigo',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ALUCHE', 'BATAN', 'CAMPAMENTO', 'CARABANCHEL', 'CASA DE CAMPO', 'EMPALME', 'EUGENIA DE MONTIJO', 'LAGO', 'VISTA ALEGRE'],
+                       node_color='grey',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,
+                       nodelist=['ANTONIO MACHADO', 'ARROYOFRESNO', 'AVDA. DE LA ILUSTRACION', 'FRANCOS RODRIGUEZ', 'LACOMA', 'PEÑAGRANDE', 'PITIS', 'VALDEZARZA'],
+                       node_color='brown',
+                       node_size=500,
+                   alpha=0.8)
+
+nx.draw_networkx_nodes(g,pos,data = True,
+                       nodelist=['BARRIO DEL PILAR', 'HERRERA ORIA', 'Mirasierra', 'PACO DE LUCIA', 'VENTILLA'],
+                       node_color='teal',
+                       node_size=500,
+                   alpha=0.8, label =True)
+
+
+labels = {}
+for node in g.nodes():
+    labels[node] = node
+
+nx.draw_networkx_labels(g,pos,labels,font_size=8,font_color='b')
+#
+nx.draw_networkx_edges(g,pos,width=1.0,alpha=0.5)
+# plt.show()
+
+print('Asortatividad: ', nx.degree_assortativity_coefficient(g))
+
+################################################
+# EXPORTAR NODOS A JSON:
+################################################
+
+json_data = json_graph.node_link_data(g)
+with open('grafo_completo.json', 'w') as file:
+    json.dump(json_data, file, indent='\t')
